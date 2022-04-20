17fdf822595379317bfafe84125b9a19decd3a2f
Yixuan Xu <yixuanxu94@outlook.com>
2022-04-20 23:47:28 +0800

    update

--- src/pages/api/v2/user.ts
+++ src/pages/api/v2/user.ts
@@ -1,29 +1,33 @@
-import { NextApiHandler } from "next";
-import { parseToken } from '../../../lib/parseToken';
+import { NextApiResponse } from "next";
+import { parseToken } from "../../../lib/parseToken";
 import prisma from "../../../lib/prisma";
-import { defaultApiHandler } from '../../../service/handler';
+import { defaultApiHandler } from "../../../service/handler";
+import { LoggerRequest } from "../../../service/log";
 
-
-const user: NextApiHandler = async (req, res) => {
+const user = async (req: LoggerRequest, res: NextApiResponse) => {
   const token = parseToken(req.headers.authorization);
-  const info = await prisma.turboToken.findUnique({
-    where: {
-      token,
-    },
-    include: {
-      user: true,
-    },
-  });
-  if (!info) {
-    res.status(401).end("Unauthorized");
-  } else {
-    res.json({
-      user: {
-        id: `team_${info.user.id}`,
-        name: info.user.name,
-        email: info.user.email,
+  try {
+    const info = await prisma.turboToken.findUnique({
+      where: {
+        token,
+      },
+      include: {
+        user: true,
       },
     });
+    if (!info) {
+      res.status(401).end("Unauthorized");
+    } else {
+      res.json({
+        user: {
+          id: `team_${info.user.id}`,
+          name: info.user.name,
+          email: info.user.email,
+        },
+      });
+    }
+  } catch (e) {
+    req.logger.error(e);
   }
 };
 
--- src/pages/api/v8/artifacts/events.ts
+++ src/pages/api/v8/artifacts/events.ts
@@ -1,21 +1,29 @@
-import is from '@sindresorhus/is';
+import is from "@sindresorhus/is";
 import { NextApiResponse } from "next";
-import prisma from '../../../../lib/prisma';
-import { defaultApiHandler } from '../../../../service/handler';
-import { TokenRequst, turboTokenMiddleWare } from '../../../../service/turbo-cache';
+import prisma from "../../../../lib/prisma";
+import { defaultApiHandler } from "../../../../service/handler";
+import {
+  TokenRequst,
+  turboTokenMiddleWare,
+} from "../../../../service/turbo-cache";
 
 const hanlder = defaultApiHandler()
   .use(turboTokenMiddleWare())
   .post<TokenRequst, NextApiResponse>(async (req, res) => {
-    if (is.nonEmptyArray(req.body)) {
-      await prisma.eventItem.createMany({
-        data: (req.body as any).map((v: any) => ({
-          ...v,
-          userId: req.userId,
-          teamId: req.teamId,
-        })),
-      });
+    try {
+      if (is.nonEmptyArray(req.body)) {
+        await prisma.eventItem.createMany({
+          data: (req.body as any).map((v: any) => ({
+            ...v,
+            userId: req.userId,
+            teamId: req.teamId,
+          })),
+        });
+      }
+    } catch (e) {
+      req.logger.error(e);
     }
+
     res.status(200).send("");
   });
 
